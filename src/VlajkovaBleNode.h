1d92874be6ec2a25dcea72c18db354827f085d7a
qsus <106612195+qsus@users.noreply.github.com>
2026-06-02 15:36:36 +0200

    add vlajková BLE functionality

---
+++ src/VlajkovaBleNode.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <Arduino.h>
+#include <NimBLEDevice.h>
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+#define MAX_CAMPS 20 
+
+struct __attribute__((packed)) CampInfo {
+    uint16_t azimuth;
+    uint16_t distance;
+    uint16_t sign;
+};
+
+class VlajkovaBleNode : public NimBLECharacteristicCallbacks {
+private:
+    NimBLEServer* pServer = nullptr;
+    NimBLECharacteristic* pCharacteristic = nullptr;
+    
+    CampInfo camps[MAX_CAMPS];
+    size_t campCount = 0;
+
+    void onWrite(NimBLECharacteristic* pChar, NimBLEConnInfo& connInfo) override {
+        std::string value = pChar->getValue();
+        size_t len = value.length();
+        size_t structSize = sizeof(CampInfo);
+        
+        // Process only if length is an exact multiple of the struct size
+        if (len > 0 && len % structSize == 0) {
+            size_t receivedCount = len / structSize;
+            
+            if (receivedCount > MAX_CAMPS) {
+                receivedCount = MAX_CAMPS;
+            }
+            
+            // Overwrite the state array with the freshest data
+            memcpy(camps, value.data(), receivedCount * structSize);
+            campCount = receivedCount;
+        }
+    }
+
+public:
+    void begin() {
+        NimBLEDevice::init("XIAO_Vlajkova");
+        pServer = NimBLEDevice::createServer();
+        NimBLEService* pService = pServer->createService(SERVICE_UUID);
+        
+        pCharacteristic = pService->createCharacteristic(
+            CHARACTERISTIC_UUID,
+            NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::WRITE_NR
+        );
+        
+        pCharacteristic->setCallbacks(this);
+        pServer->start();
+        
+        NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
+        pAdvertising->addServiceUUID(SERVICE_UUID);
+        pAdvertising->start();
+    }
+
+    // Return read-only pointer to the current state of camps
+    const CampInfo* getCamps() const {
+        return camps;
+    }
+
+    // Return current number of active camps
+    size_t getCampCount() const {
+        return campCount;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <Arduino.h>
+#include "VlajkovaBleNode.h" 
+
+VlajkovaBleNode bleNode;
 
 void setup() {
-	Serial.begin(115200);
+    Serial.begin(115200);
+    bleNode.begin();
 }
 
 void loop() {
-	Serial.println("Hi");
-	delay(5);
+    // Read camps
+    size_t count = bleNode.getCampCount();
+    const CampInfo* activeCamps = bleNode.getCamps();
+    for (size_t i = 0; i < count; i++) { // For each camp
+        uint16_t azimuth = activeCamps[i].azimuth;
+		uint16_t distance = activeCamps[i].distance;
+		Serial.println(azimuth + distance);
+    }
+	Serial.println();
+
+    delay(10); 
 }
